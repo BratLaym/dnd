99ce6f1e4c6b8b840d5bdb2cb49704a1ab29ae1a
Igor <igor.mamaev1@gmail.com>
2025-11-10 13:05:39 +0300

    adminbot

--- adminbot/handlers/start.py
+++ adminbot/handlers/start.py
@@ -2,9 +2,9 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-start_router = Router()
+router = Router()
 
 
-@start_router.message(CommandStart())
+@router.message(CommandStart())
 async def start_command(message: Message) -> None:
     await message.answer(text="Этот бот пока что ничего не делает")
